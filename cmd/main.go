e07ccf29805c0a2fdc6d4cfc306a01beef157c3a
Mihail Chapenko <mihail.chapenko@gmail.com>
2021-11-09 21:42:15 +0200

    fixes

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/MihailChapenko/lets-go-chat-app/cmd/server"
 	"github.com/MihailChapenko/lets-go-chat-app/internal/config"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	config.Init("../internal/config/config.yaml")
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	config.Init(filepath.Join(pwd, "../internal/config/config.yaml"))
 	//db.Init(config.Get().DB)
 	server.Init()
 }
